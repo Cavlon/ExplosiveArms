720a68e98dba27c2fab478ccf21ef99e133d4e5f
Cavlon <cavlon3006@gmail.com>
2021-06-24 19:19:06 +0100

    All Levels

--- Assets/SpawnLevel.cs
+++ Assets/SpawnLevel.cs
@@ -5,23 +5,38 @@ public class SpawnLevel : MonoBehaviour
 {
 
     public List<LevelInfo> levels = new List<LevelInfo>();
+    private int levelNo;
 
     void Start()
-    {        
+    {
+        levelNo = 0;
         NewLevel();
     }
 
 
     public void NewLevel()
     {
-        int randVal = Random.Range(0, levels.Count);
-        Instantiate(levels[randVal]);
-        levels.RemoveAt(randVal);
+        if (levelNo < 9)
+        {
+            NormalLevel();
+            levelNo += 1;
+        }
+        else
+        {
+            print("Boss Level Here");
+        }  
         Invoke("ReScan", 1f);
     }
 
     private void ReScan()
     {
         AstarPath.active.Scan();
+    }
+
+    private void NormalLevel()
+    {
+        int randVal = Random.Range(0, levels.Count);
+        Instantiate(levels[randVal]);
+        levels.RemoveAt(randVal);
     }
 }
